da4c2b5d263dc5e3ae2a8bec0d2b225813c9687c
Семья <@WIN-PC>
2022-12-02 11:33:43 +0400

    Добавьте файлы проекта.

---
+++ IControls.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore.Update.Internal;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Notepad
+{
+    internal interface IControls
+    {
+        public void Update();
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Notepad
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void openAddSomeText(object sender, MouseButtonEventArgs e)
+        {
+            AddSomeText addSomeText = new();
+            addSomeText.ShowDialog();
+        }
+        public void Update()
+        {
+            DataBaseContext dataBaseContext = new();
+            ListSomeText.ItemsSource = dataBaseContext.DataBases.ToList();
+        }
+        private void grid_Loaded(object sender, RoutedEventArgs e)
+        {
+            Update();
+        }
+    }
+}
